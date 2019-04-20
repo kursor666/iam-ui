4b25047276af0ee82408643662dd6ebd9d2300c6
kursor666 <vladskater73@gmail.com>
2019-04-21 00:58:15 +0700

    Users list.

--- src/Components/Users/List/UsersList.js
+++ src/Components/Users/List/UsersList.js
@@ -1,6 +1,37 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import Table from "react-bootstrap/Table";
 
 export const UsersList = (props) => {
+    let [users, setUsers] = useState([]);
 
-    return (<label>v = {props.name}</label>);
+    useEffect(() => {
+        let originalUsers = [
+            {id: '1', name: 'test1', surname: 'test'},
+            {id: '2', name: 'test2', surname: 'test2'},
+            {id: '3', name: 'test3', surname: 'test3'}
+        ];
+        if (!props.name) setUsers(originalUsers);
+        else setUsers(users.filter(value => value.name.includes(props.name)))
+    });
+
+    return (
+        <Table responsive>
+            <thead>
+            <tr>
+                <th>id</th>
+                <th>name</th>
+                <th>surname</th>
+            </tr>
+            </thead>
+            <tbody>
+            {users.map((user, key) => (
+                <tr key={key}>
+                    <th>{user.id}</th>
+                    <th>{user.name}</th>
+                    <th>{user.surname}</th>
+                </tr>
+            ))}
+            </tbody>
+        </Table>
+    );
 };
